b4b23711a132a66334f0988ad75d9d8cb1461b73
Alexander Chang <alexanderchang208@gmail.com>
2025-08-24 16:56:21 -0400

    finish menu items

--- camera.js
+++ camera.js
@@ -14,6 +14,12 @@ windowContext.scale(dpr, dpr);
 const centerX = rect.width / 2;
 const centerY = rect.height / 2;
 
+function clearWindow () {
+  windowContext.clearRect(0, 0, rect.width, rect.height);
+}
+
+// text functions start
+
 function writeCenterText (textContent, textColor, textFont, textSize) {
   document.fonts.load(String(textSize) + "px " + textFont).then(() => {
     windowContext.textAlign = "center";
@@ -23,3 +29,42 @@ function writeCenterText (textContent, textColor, textFont, textSize) {
     windowContext.fillText(textContent, centerX, centerY);
   });
 }
+
+function writeManualCenterParagraph(ctx, text, lineHeight, font, color) {
+  const rect = ctx.canvas.getBoundingClientRect();
+
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.textAlign = "center";
+  ctx.textBaseline = "top";
+
+  const lines = text.split("\n");
+  const blockHeight = lines.length * lineHeight;
+  let y = (rect.height - blockHeight) / 2;
+
+  lines.forEach((line) => {
+    ctx.fillText(line, rect.width / 2, y);
+    y += lineHeight;
+  });
+}
+
+// text functions end
+
+// image functions start
+
+function drawCenterImage (imgSrc, targetWidth, targetHeight) {
+  const newImage = new Image();
+  newImage.src = imgSrc;
+
+  newImage.onload = () => {
+    const width = targetWidth || newImage.width;
+    const height = targetHeight || newImage.height;
+
+    const x = (rect.width - width) / 2;
+    const y = (rect.height - height) / 2;
+
+    windowContext.drawImage(newImage, x, y, width, height);
+  }
+}
+
+// image functions end
---
+++ game_events.js
@@ -0,0 +1,135 @@
+let gameEventLocation = 0;
+
+/*
+disabled: denies user interaction
+0 is the very, very beginning of the game. (Loading screen)
+1 is the title screen select menu
+*/
+
+// the very start
+writeCenterText("Loading assets", "white", "FSEX300", 25);
+setTimeout(function () {
+  clearWindow();
+  writeCenterText("Press any key to continue", "powderblue", "FSEX300", 25);
+}, 3000);
+
+function secondLoadingScreen () {
+  $(gameWindow).fadeOut(2000);
+
+  gameEventLocation = "disabled";
+  setTimeout(function () {
+    clearWindow();
+    writeCenterText("Programmed by Alexander Chang", "white", "FSEX300", 25);
+
+    $(gameWindow).fadeIn(2000);
+
+    setTimeout(function () {
+      $(gameWindow).fadeOut(2000);
+
+      setTimeout(function () {
+        clearWindow();
+        // writeCenterText("Test", "white", "FSEX300", 25);
+        drawCenterImage("../Visigoth/assets/start/frame1.webp");
+
+        $(gameWindow).fadeIn(2000);
+
+        setTimeout(function () {
+          flickerStartFrames();
+        }, 2000);
+      }, 2000);
+    }, 4000);
+  }, 2000);
+}
+
+function flickerStartFrames () {
+  let flickerStartFramePosition = 0;
+  const flickerInterval = setInterval(function () {
+    switch (flickerStartFramePosition) {
+      case 0:
+        clearWindow();
+        drawCenterImage("../Visigoth/assets/start/frame2.webp");
+        flickerStartFramePosition = 1;
+        break;
+      case 1:
+        clearWindow();
+        drawCenterImage("../Visigoth/assets/start/frame1.webp");
+        flickerStartFramePosition = 0;
+        break;
+    }
+  }, 50);
+
+  setTimeout(function () {
+    clearInterval(flickerInterval);
+    clearWindow();
+    drawCenterImage("../Visigoth/assets/start/frame1.webp");
+
+    $(gameWindow).fadeOut(1000);
+
+    setTimeout(function () {
+      clearWindow();
+      gameEventLocation = 1;
+
+      $(gameWindow).fadeIn(1000);
+
+      setTimeout(function () {
+        loadMenuItems(0);
+      }, 1);
+    }, 1000);
+  }, 1500);
+}
+
+function loadMenuItems (arrowWhich) {
+  switch (arrowWhich) {
+    case 0:
+      writeManualCenterParagraph(
+        windowContext, 
+        "►Start a new game.\nContinue.",
+        30,
+        "24px FSEX300",
+        "white"
+      );
+      break;
+    case 1:
+      writeManualCenterParagraph(
+        windowContext, 
+        "Start a new game.\n►Continue.",
+        30,
+        "24px FSEX300",
+        "white"
+      );
+      break;
+  }
+}
+
+let userTitleMenuSelectPosition = 0; // Start new game
+$(document).on("keydown", function (event) {
+  switch (gameEventLocation) {
+    case 0:
+      secondLoadingScreen();
+      break;
+    case 1:
+      switch (userTitleMenuSelectPosition) {
+        case 0:
+          switch (event.which) {
+            case 39:
+            case 40:
+              clearWindow();
+              userTitleMenuSelectPosition = 1;
+              loadMenuItems(1);
+              break;
+          }
+          break;
+        case 1:
+          switch (event.which) {
+            case 38:
+            case 37:
+              clearWindow();
+              userTitleMenuSelectPosition = 0;
+              loadMenuItems(0);
+              break;
+          }
+          break;
+      }
+      break;
+  }
+});
