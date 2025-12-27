8347fcf2e7b480a3f5b120c66b28fcd9c3f1e721
Alexander Chang <alexanderchang208@gmail.com>
2025-12-26 21:29:10 -0600

    e

--- alpha.js
+++ alpha.js
@@ -2,6 +2,115 @@ let alpha_pos = null; // alpha pos = 1 achieves the same result
 let alpha_keypos = 0;
 let alpha_dpos = 0;
 
+let current_alpha_audio = 0;
+let current_battle_alpha_audio = 0;
+
+let current_doorway = 0;
+let current_path_loc = "forest";
+let path_alpha_lim = 10;
+
+const pointerWindow = document.getElementById("alpha-window");
+const pointer = document.getElementById("pointer");
+
+const alphaBatOptions = document.getElementById("alpha-bat-options");
+const alphaBatName = document.getElementById("alpha-bat-name");
+const alphaBatStatus = document.getElementById("alpha-bat-status");
+
+let battleMenuOption_alpha = 0;
+const alphaBatAtk = document.getElementById("alpha-bat-atk");
+const alphaBatHeal = document.getElementById("alpha-bat-heal");
+const alphaBatEsc = document.getElementById("alpha-bat-esc");
+
+let alpha_hp = 30;
+let alpha_atk_range = 5;
+
+let drawEnemy = "";
+let drawEnemy_src = "";
+let battleFrame = "";
+let enemyAtkRange = 0;
+let enemyHp = 0;
+
+function doorwayBattleInit () {
+  current_alpha_audio.pause();
+  current_battle_alpha_audio = playLoopedAudio("../Visigoth/battle/music/alpha.mp3");
+
+  setTimeout(function () {
+    clearWindow();
+    clearAllWindows();
+
+    alphaBatAtk.innerText = "> Attack";
+    alphaBatHeal.innerText = "Heal";
+    alphaBatEsc.innerText = "Escape";
+    battleMenuOption_alpha = 0;
+
+    switch (current_path_loc) {
+      case "forest":
+        battleFrame = "../Visigoth/prequel/frame6.jpg";
+        drawEnemy = "Rabid Fox";
+        drawEnemy_src = "../Visigoth/prequel/rabidfox.png";
+        enemyAtkRange = 3;
+        break;
+    }
+    drawImageLeft(battleFrame, 800, 500);
+    
+    setTimeout(function () {
+      createWindow("battleMessage", "A " + drawEnemy + " appears!", 0, 0);
+      drawCenterImage(drawEnemy_src, 341, 341);
+
+      pointerWindow.style.display = "block";
+      alphaBatOptions.style.display = "block";
+      alphaBatStatus.style.display = "block";
+
+      playClonedAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+    }, 1000);
+
+    $(gameWindow).slideDown(2000);
+
+    setTimeout(function () {
+      alpha_pos = 4;
+    }, 2000);
+  }, 2000);
+}
+
+function attackSelect_alpha () {
+  const attackAnim_interval = setInterval(function () {
+    drawImageLeft(battleFrame, 800, 500);
+    setTimeout(function () {
+      drawCenterImage(drawEnemy_src, 341, 341);
+    }, 125);
+  }, 150);
+
+  setTimeout(function () {
+    clearInterval(attackAnim_interval);
+  }, 300);
+
+  playClonedAudio("../Visigoth/battle/dspistol.wav");
+
+  alpha_pos = null;
+  alphaBatOptions.style.display = "none";
+
+  setTimeout(function () {
+    
+  }, 1000);
+}
+
+function doorwaySelect () {
+  $(gameWindow).slideUp(2000);
+  alpha_pos = null;
+  pointerWindow.style.display = "none";
+
+  pointer.style.display = "none";
+  pointer.style.left = "180px";
+
+  let railwaySpike = Math.floor(Math.random() * 3);
+
+  switch (railwaySpike) {
+    case 0:
+      doorwayBattleInit();
+      break;
+  }
+}
+
 setTimeout(function () {
   menuItemColors = "black";
   gameWindow.style.display = "none";
@@ -52,6 +161,7 @@ function loadAlphaMenu (menPos) {
 
 function loadInIntro () {
   playClonedAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+  current_alpha_audio = playLoopedAudio("../Visigoth/assets/audio/fw4.mp3");
 
   $(gameWindow).fadeOut(1500);
   alpha_pos = 1;
@@ -181,7 +291,7 @@ function jaydenEncounter_a1 () {
 }
 
 function jaydenEncounter_a2 () {
-  playAudio("../Visigoth/prequel/crowd.mp3", 800, 500);
+  playAudio("../Visigoth/prequel/crowd.mp3");
   alpha_dpos = null;
   clearAllWindows();
   setTimeout(function () {
@@ -195,8 +305,61 @@ function jaydenEncounter_a2 () {
 
 // end jayden encounter
 
+// start forest
+
+function forestDialogue_a1 () {
+  $(gameWindow).fadeOut(2000);
+  clearAllWindows();
+  alpha_dpos = null;
+
+  setTimeout(function () {
+    clearWindow();
+    drawImageLeft("../Visigoth/prequel/forest.jpg", 800, 500);
+
+    $(gameWindow).slideDown(2000);
+
+    setTimeout(function () {
+      drawCenterImage("../Visigoth/prequel/doors1.png", 556, 186);
+
+      setTimeout(function () {
+        createWindow("dialogue", `Jack: "We came to the meeting in your car. It's at the edge of this forest."`, 0, 0);
+
+        setTimeout(function () {
+          alpha_dpos = 10;
+        }, getWaitTextTime(`Jack: "We came to the meeting in your car. It's at the edge of this forest."`));
+      }, 300)
+    }, 2000);
+  }, 2000);
+}
+
+function forestDialogue_a2 () {
+  alpha_dpos = null;
+  clearAllWindows();
+  createWindow("dialogue", `Jack: "Anthony and I will see if there's anybody at town hall that we can help. You get the car and drive back here!"`, 0, 0);
+
+  setTimeout(function () {
+    alpha_dpos = 11;
+  }, getWaitTextTime(`Jack: "Anthony and I will see if there's anybody at town hall that we can help. You get the car and drive back here!"`));
+}
+
+function forestDialogue_a3 () {
+  alpha_dpos = null;
+  clearAllWindows();
+  createWindow("battleMessage", "Select the pathway you wish to travel down.", 0, 0);
+
+  pointerWindow.style.display = "block";
+  pointer.style.display = "block";
+
+  pointer.style.left = "180px";
+  pointer.style.bottom = "100px";
+  playAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+  alpha_pos = 3;
+}
+
+// end forest
+
 function getWaitTextTime (text) {
-  return (text.length * 50 - 480);
+  return (text.length * 50 - 1280);
 }
 
 $(document).on("keydown", function (event) {
@@ -211,6 +374,23 @@ $(document).on("keydown", function (event) {
               break;
           }
           break;
+        case 3:
+          playClonedAudio("../Visigoth/assets/audio/sfx/vgmenuselect.ogg");
+          switch (current_doorway) {
+            case 0:
+              current_doorway = 1;
+              pointer.style.left = "360px";
+              break;
+            case 1:
+              current_doorway = 2;
+              pointer.style.left = "560px";
+              break;
+            case 2:
+              current_doorway = 0;
+              pointer.style.left = "180px";
+              break;
+          }
+          break;
       }
       break;
     case 37:
@@ -220,6 +400,71 @@ $(document).on("keydown", function (event) {
             case 1:
               loadAlphaMenu(0);
               alpha_keypos = 0;
+              break;
+          }
+          break;
+        case 3:
+          playClonedAudio("../Visigoth/assets/audio/sfx/vgmenuselect.ogg");
+          switch (current_doorway) {
+            case 0:
+              current_doorway = 2;
+              pointer.style.left = "560px";
+              break;
+            case 1:
+              current_doorway = 0;
+              pointer.style.left = "180px";
+              break;
+            case 2:
+              current_doorway = 1;
+              pointer.style.left = "360px";
+              break;
+          }
+          break;
+      }
+      break;
+    case 40:
+      switch (alpha_pos) {
+        case 4:
+          playClonedAudio("../Visigoth/assets/audio/sfx/vgmenuselect.ogg");
+          switch (battleMenuOption_alpha) {
+            case 0:
+              battleMenuOption_alpha = 1;
+              alphaBatAtk.innerText = "Attack";
+              alphaBatHeal.innerText = "> Heal";
+              break;
+            case 1:
+              battleMenuOption_alpha = 2;
+              alphaBatHeal.innerText = "Heal";
+              alphaBatEsc.innerText = "> Escape";
+              break;
+            case 2:
+              battleMenuOption_alpha = 0;
+              alphaBatEsc.innerText = "Escape";
+              alphaBatAtk.innerText = "> Attack";
+              break;
+          }
+          break;
+      }
+      break;
+    case 38:
+      switch (alpha_pos) {
+        case 4:
+          playClonedAudio("../Visigoth/assets/audio/sfx/vgmenuselect.ogg");
+          switch (battleMenuOption_alpha) {
+            case 0:
+              battleMenuOption_alpha = 2;
+              alphaBatAtk.innerText = "Attack";
+              alphaBatEsc.innerText = "> Escape";
+              break;
+            case 1:
+              battleMenuOption_alpha = 0;
+              alphaBatHeal.innerText = "Heal";
+              alphaBatAtk.innerText = "> Attack";
+              break;
+            case 2:
+              battleMenuOption_alpha = 1;
+              alphaBatEsc.innerText = "Escape";
+              alphaBatHeal.innerText = "> Heal";
               break;
           }
           break;
@@ -267,6 +512,27 @@ $(document).on("keydown", function (event) {
             case 8:
               jaydenEncounter_a2();
               playClonedAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+              break;
+            case 9:
+              forestDialogue_a1();
+              playClonedAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+              break;
+            case 10:
+              forestDialogue_a2();
+              playClonedAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+              break;
+            case 11:
+              forestDialogue_a3();
+              playClonedAudio("../Visigoth/assets/audio/sfx/coin7.wav");
+              break;
+          }
+          break;
+        case 3:
+          doorwaySelect();
+          break;
+        case 4:
+          switch (battleMenuOption_alpha) {
+            case 0:
               break;
           }
           break;
